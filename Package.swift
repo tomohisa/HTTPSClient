5d40e2d0eb723a1e610969e842cdf08acfd22e15
Tomohisa Takaoka <tomohisa@jtechs.com>
2016-04-28 03:20:24 +0900

    testing for https crash
    workaround for
    
    https://github.com/Zewo/Zewo/issues/81#issuecomment-215141561

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "HTTPSClient",
     dependencies: [
-        .Package(url: "https://github.com/VeniceX/TCPSSL.git", majorVersion: 0, minor: 5),
+        .Package(url: "https://github.com/tomohisa/TCPSSL.git", majorVersion: 0, minor: 5),
         .Package(url: "https://github.com/Zewo/HTTPParser.git", majorVersion: 0, minor: 5),
         .Package(url: "https://github.com/Zewo/HTTPSerializer.git", majorVersion: 0, minor: 5),
     ]
